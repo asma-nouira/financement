4fb913fc73b41904253bf83a6dd027ed3bfb7701
asma <asma@viruscomm.ca>
2026-06-04 10:45:52 +0100

    home page

--- wp-content/themes/vsc-theme/footer.php
+++ wp-content/themes/vsc-theme/footer.php
@@ -17,23 +17,22 @@
 
       <!-- Navigation centre -->
       <nav class="site-footer__nav">
-         <div class="left-col"> 
-        <ul class="site-footer__nav-col">
+          
+        <ul class="site-footer__nav-col first">
           <li><a href="/a-propos">À PROPOS</a></li>
           <li><a href="/processus-pour-faire-un-demande">PROCESSUS POUR FAIRE UN DEMANDE</a></li>
         </ul>
-		  <div class="site-footer__phone-wrap">
-          <a href="tel:5140000000" class="site-footer__phone">(514) 000-0000</a>
-        </div>
 
-         </div>
         <ul class="site-footer__nav-col">
           <li><a href="/faq">FAQ</a></li>
           <li><a href="/nous-joindre">CONTACTEZ-NOUS</a></li>
         </ul>
 
         <!-- Téléphone sous les liens -->
-      
+        <div class="site-footer__phone-wrap">
+          <a href="tel:5140000000" class="site-footer__phone">(514) 000-0000</a>
+        </div>
+
       </nav>
 
     </div><!-- .site-footer__inner -->
