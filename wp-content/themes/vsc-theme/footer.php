bcd944352769b4203b9f9f0fef5a13947d027f25
asma <asma@viruscomm.ca>
2026-06-23 10:50:40 +0100

    QA

--- wp-content/themes/vsc-theme/footer.php
+++ wp-content/themes/vsc-theme/footer.php
@@ -12,7 +12,7 @@
 
       <!-- Logo -->
       <div class="site-footer__logo">
-        <a href="/"><img src="/wp-content/uploads/2026/06/Logo-footer.png" alt="Fianancement sourire"></a>
+        <a href="/"><img src="/wp-content/uploads/2026/06/Logo-footer.png" alt="Financement sourire"></a>
       </div>
 
       <!-- Navigation centre -->
